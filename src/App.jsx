e7fad8eb1565d12fcfb9df5807892d43ee20100d
Valeriia Nedashkivska <nedashkivskavaleriia@gmail.com>
2025-04-22 22:56:28 +0300

    Add files SignUp

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
 
 function App() {
-  return <SignInPage></SignInPage>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import '../styles/SignInStyle.css';
+import SignInForm from '../components/SignInForm';
+
+export default function SignUpPage() {
+  return (
+    <>
+      <h1>wow</h1>
+    </>
+  );
+}
