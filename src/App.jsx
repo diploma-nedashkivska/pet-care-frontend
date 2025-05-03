05be4c97de379aaf2b4e2683d23d9d2d1a130171
Valeriia Nedashkivska <nedashkivskavaleriia@gmail.com>
2025-05-03 22:44:50 +0300

    Add ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
 import Start from './pages/Start';
-
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/profile" element={<Start />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Start />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/" element={<SignInPage />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
       </Routes>
