169af4e127ed74c474ace618af4985bde53469f4
Valeriia Nedashkivska <nedashkivskavaleriia@gmail.com>
2025-05-03 22:37:20 +0300

    Add AuthContext

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles/index.css';
 import './i18n.jsx';
+import { AuthProvider } from './components/AuthContext';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <AuthProvider>
+    <App/>
+  </AuthProvider>
 );
